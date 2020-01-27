413df578cbdfbe0e9d43071e3afb782b8875996c
louisebathan <60352152+louisebathan@users.noreply.github.com>
2020-01-28 01:09:39 +0800

    Create 11914130_exer1.py

---
+++ 11914130_exer1.py
@@ -0,0 +1,14 @@
+cd /path/to/my/codebase
+git init      
+git add
+
+Author: Louise Alecxandre R. Bathan
+Date: January 27, 2020
+Course: LBYCPA1
+
+Program Description
+is a version control system for tracking changes in computer files and coordinating work on those files among multiple people.
+It is primarily used for source code management in software development,
+but it can be used to keep track of changes in any set of files
+
+
